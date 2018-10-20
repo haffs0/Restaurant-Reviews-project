aa903f0d8a30767d684f49a4c67162665fdb89ae
Abu Afeez <abuafeezabolarinwa@yahoo.com>
2018-10-20 18:57:53 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -176,9 +176,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = "Image of the restaurant "+ restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -191,7 +192,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = 'View details of the restaurant ' + restaurant.name;
   more.href = DBHelper.urlForRestaurant(restaurant);
   more.tabIndex = '3'
   li.append(more)
